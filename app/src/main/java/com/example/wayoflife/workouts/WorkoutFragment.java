567d0ecf0d9bcf2e47a4742fd3dd26fa45349297
mirkomatellini <m.matellini@campus.unimib.it>
2021-05-17 15:59:17 +0200

    Inizializzato Fragment

---
+++ app/src/main/java/com/example/wayoflife/workouts/WorkoutFragment.java
@@ -0,0 +1,22 @@
+package com.example.wayoflife.workouts;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wayoflife.R;
+
+import androidx.fragment.app.Fragment;
+
+public class WorkoutFragment extends Fragment {
+
+    private static final String TAG = "WorkoutFragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_content, container, false);
+    }
+}
